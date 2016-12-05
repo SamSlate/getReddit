a5c9464de6883cba988ecbf32a2a1f11bb8971f3
SamSlate <burningbenjamins+samslate@gmail.com>
2016-12-05 15:38:33 -0600

    test

--- getReddit.js
+++ getReddit.js
@@ -1,3 +1,9 @@
+//get user
+//get page
+//vote 
+//comment
+
+
 //get
 function get(page, meth, dat, callback, err){
 	$.ajax({
@@ -7,8 +13,8 @@ function get(page, meth, dat, callback, err){
 			timeout: 6000,
 			async: true,
 		success: function (response) {
-			if(q.beta == 'debug') console.log('  success ('+meth+', '+dat+'):\n\t',page);
-			if(q.beta == 'debug') console.log(response);
+			console.log('  success ('+meth+', '+dat+'):\n\t',page);
+			console.log(response);
 			callback(response);
 		},
 		error: function (request){
